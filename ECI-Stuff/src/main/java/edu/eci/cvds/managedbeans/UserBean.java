244714aefe70f93529214db57a197ec31dcfc0f5
Francisco Rojas M <francisco.rojas-m@mail.escuelaing.edu.co>
2022-04-13 09:22:42 -0500

    fix de ejemplo

--- ECI-Stuff/src/main/java/edu/eci/cvds/managedbeans/UserBean.java
+++ ECI-Stuff/src/main/java/edu/eci/cvds/managedbeans/UserBean.java
@@ -4,8 +4,10 @@ import edu.eci.cvds.entities.User;
 import edu.eci.cvds.services.ECIStuffServices;
 import edu.eci.cvds.services.ServicesException;
 
+import javax.faces.application.FacesMessage;
 import javax.faces.bean.ManagedBean;
 import javax.faces.bean.RequestScoped;
+import javax.faces.context.FacesContext;
 import javax.inject.Inject;
 import java.util.List;
 
@@ -21,12 +23,13 @@ public class UserBean extends BasePageBean {
 	private ECIStuffServices eciStuffServices;
 
 	public void createUser() throws Exception {
-		System.out.println("Hola");
 		try {
 			eciStuffServices.createUsers();
 		} catch (ServicesException ex) {
 			throw ex;
 		}
 	}
+
+
 
 }
