08f9d64b3dd82def1afec9d66455226a43e561ba
Francisco Rojas M <francisco.rojas-m@mail.escuelaing.edu.co>
2022-05-08 00:07:51 -0500

    mensajes de error y estilos login

--- src/main/java/edu/eci/cvds/managedbeans/ValidatorsBean.java
+++ src/main/java/edu/eci/cvds/managedbeans/ValidatorsBean.java
@@ -13,14 +13,24 @@ import javax.faces.validator.ValidatorException;
 public class ValidatorsBean {
     public void validateEmail(FacesContext arg0, UIComponent arg1, Object arg2)
             throws ValidatorException {
-        if (((String)arg2).trim().isEmpty()) {
+            String emailInserted = (String)arg2;
+        if ((emailInserted).trim().isEmpty()) {
             throw new ValidatorException(new FacesMessage("Credencial correo vacía o nula verifiqué"));
+        } else if(emailInserted.trim().length() < 3 ){
+            throw new ValidatorException(new FacesMessage("Credencial correo muy corta, proporcione mas de 3 caracteres"));
+        } else if(emailInserted.trim().length() > 60){
+            throw new ValidatorException(new FacesMessage("Numero de caracteres exedidos en credencial correo"));
         }
     }
     public void validatePassword(FacesContext arg0, UIComponent arg1, Object arg2)
             throws ValidatorException {
-        if (((String)arg2).trim().isEmpty()) {
+        String passwordInserted = (String)arg2;
+        if ((passwordInserted).trim().isEmpty()) {
             throw new ValidatorException(new FacesMessage("Credencial contraseña vacía o nula verifiqué"));
+        } else if(passwordInserted.trim().length() < 3 ){
+            throw new ValidatorException(new FacesMessage("Credencial contraseña muy corta, proporcione mas de 3 caracteres"));
+        } else if(passwordInserted.trim().length() > 60) {
+            throw new ValidatorException(new FacesMessage("Numero de caracteres exedidos en credencial contraseña"));
         }
     }
 }
